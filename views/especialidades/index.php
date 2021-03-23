42a6d1482015a7255c218706016c208653cb8b64
Hericson Ramos <hericson@bwfti.com.br>
2021-03-23 20:44:10 -0300

    First Commit

---
+++ views/especialidades/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this yii\web\View */
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+
+<h1>Especialidades</h1>
+<?php $form = ActiveForm::begin();?>
+<?php echo $form->errorSummary($Vmodel);      ?>
+
+    <?= $form->field($Vmodel,'Titulo') ?>
+    <?= $form->field($Vmodel,'SubTitulo') ?>
+    <?= $form->field($Vmodel,'texto')->textArea() ?>
+    <?= Html::submitButton("Enviar",['class' => 'btn btn-success'])?>
+<?php ActiveForm::end();?>
